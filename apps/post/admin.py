9dfb6fe12a727b2cebaa1f5e58fc8c1b31bc4ae1
Diogo Lima <diogo@dr6.com.br>
2020-06-09 01:03:59 -0300

    admin

--- apps/post/admin.py
+++ apps/post/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("titulo",)}
+    list_display = ["titulo", "slug", "created_at", "modified_at"]
+    list_filter = ("created_at", "modified_at")
+    search_fields = ("titulo", "texto", "autor__username", "autor__first_name", "autor__last_name")
+
+    def save_model(self, request, obj, form, change):
+        if not obj.autor_id:
+            obj.autor = request.user
+        super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(autor=request.user)
+
+    def get_list_display(self, request):
+        if request.user.is_superuser:
+            return self.list_display + ["autor"]
+        return self.list_display
--- apps/post/models.py
+++ apps/post/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.conf import settings
 
+
 class Post(models.Model):
-    autor = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, editable=False,
+                              on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    slug = models.SlugField("link", unique=True)
+    texto = models.TextField()
+    created_at = models.DateTimeField("criado", auto_now_add=True)
+    modified_at = models.DateTimeField("modificado", auto_now=True)
