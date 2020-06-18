cc8e744aa5c93db729f050f7a8dc71f9efe29ffa
Diogo Lima <diogo@dr6.com.br>
2020-06-18 18:55:55 -0300

    avatar

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,10 +1,18 @@
+import hashlib
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from taggit.models import TagBase, GenericTaggedItemBase
 
 
+def upload_directory_path(instance, filename):
+    return 'users/{}/{}.{}'.format(
+        instance.username, hashlib.sha224(filename.encode()).hexdigest(),
+        filename.split(".")[-1:]
+    )
+
 class User(AbstractUser):
     apresentacao = models.CharField(max_length=120)
+    avatar = models.ImageField(null=True, upload_to=upload_directory_path)
 
 
 class TagWithHits(TagBase):
